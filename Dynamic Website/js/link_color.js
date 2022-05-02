7ce63053f66983dc63e8e7d24c78403d48a6156b
Aneurinmind <50070328+Aneurinmind@users.noreply.github.com>
2022-05-03 00:36:42 +0800

    Add files via upload

---
+++ Dynamic Website/js/link_color.js
@@ -0,0 +1,11 @@
+// <script type='text/javascript'>
+
+// function SetColor(obj) {
+// 	var aList = document.getElementsByTagName("a");
+//             for (var i = 0, len = aList.length; i < len; i++) {
+//                 aList[i].removeAttribute("style");
+//             }
+//             obj.style.color = "red";  /所要设置的颜色
+//         }
+
+// </script>
